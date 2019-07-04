eb98013a53e136ad3ca57f4dc147461e9fed971a
Rajat Mangal <rajatmangal@Rajats-MBP.hitronhub.home>
2019-07-03 22:45:45 -0700

    StoryBrain  Class Added

--- lib/story.dart
+++ lib/story.dart
@@ -3,9 +3,9 @@ class Story {
   String choice1;
   String choice2;
 
-  Story(String title, String one,String two) {
-    storyTitle = title;
-    choice1 = one;
-    choice2 = two;
+  Story({String storyTitle, String choice1,String choice2}) {
+    storyTitle = this.storyTitle;
+    choice1 = this.choice1;
+    choice2 = this.choice2;
   }
 }
