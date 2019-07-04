bb9b0dd24d4b7ad8e9e18e9ab0c386754f65dbe6
Rajat Mangal <rajatmangal@Rajats-MBP.hitronhub.home>
2019-07-03 22:55:31 -0700

    First Story Added

--- lib/story.dart
+++ lib/story.dart
@@ -4,8 +4,8 @@ class Story {
   String choice2;
 
   Story({String storyTitle, String choice1,String choice2}) {
-    storyTitle = this.storyTitle;
-    choice1 = this.choice1;
-    choice2 = this.choice2;
+    this.storyTitle = storyTitle;
+    this.choice1 = choice1;
+    this.choice2 = choice2;
   }
 }
